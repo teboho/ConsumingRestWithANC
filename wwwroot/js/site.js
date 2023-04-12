6f221779050836e343401087f3b5d9f546049672
Teboho Makhalemele <217020478@student.uj.ac.za>
2023-04-12 23:38:52 +0200

    Reload in 20 sec

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Reload page after 30 seconds
+setTimeout(function () {
+    location.reload();
+}
+, 20000);
